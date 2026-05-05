c5739cb15671c532a1cd1095dfdec112304b045f
YO <N00199817@upn.pe>
2026-05-04 19:59:04 -0500

    uno

---
+++ src/semana7/Semana7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana7;
+
+/**
+ *
+ * @author UCC20411
+ */
+public class Semana7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
